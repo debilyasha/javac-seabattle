53c3f2d4b96b06e6812a5fb285aabd4d78d52ba1
Кирилл Технологичный <dev.kirilltech@yandex.ru>
2018-08-14 01:39:56 +1000

    :3

---
+++ src/DotCom.java
@@ -0,0 +1,108 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public String checkYourSelf(String userInput) {
+        String res = "Мимо";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                res = "Потопил";
+                System.out.printf("Ой, вы потопили %s!\n", name);
+            } else {
+                res = "Попал";
+            }
+        }
+        return res;
+    }
+}
+
+class GameHelper {
+    private static final String alphabet = "абвгдеё";
+    private int gridLength = 7;
+    private int gridSize = 49;
+    private int[] grid = new int[gridSize];
+    private int comCount = 0;
+
+    public String getUserInput(String text) {
+        String input = null;
+        System.out.print(text + ": ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            input = is.readLine();
+            if (input.length() == 0) {
+                return null;
+            }
+        } catch (IOException ex) {
+            System.out.println("IOExpection: " + ex);
+        }
+        return input.toLowerCase();
+    }
+
+    public ArrayList<String> placeDotCom(int comSize) {
+        ArrayList<String> alphaCells = new ArrayList<>();
+        String[] alphacoords = new String[comSize];
+        String temp = null;
+        int[] coords = new int[comSize];
+        int attempts = 0;
+        boolean succes = false;
+        int location = 0;
+
+        comCount++;
+        int incr = 1;
+        if ((comCount % 2) == 1) {
+            incr = gridLength;
+        }
+        while (!succes & attempts++ < 200) {
+            location = (int) (Math.random() * gridSize);
+            int x = 0;
+            succes = true;
+            while (succes && x < comSize) {
+                if (grid[location] == 0) {
+                    coords[x++] = location;
+                    location += incr;
+                    if (location >= gridSize) {
+                        succes = false;
+                    }
+                    if (x > 0 && (location % gridLength == 0)) {
+                        succes = false;
+                    }
+                } else {
+                    succes = false;
+                }
+            }
+        }
+
+        int x = 0;
+        int row = 0;
+        int column = 0;
+
+        while (x < comSize) {
+            grid[coords[x]] = 1;
+            row = (int) (coords[x] / gridLength);
+            column = coords[x] % gridLength;
+            temp = String.valueOf(alphabet.charAt(column));
+            alphaCells.add(temp.concat(Integer.toString(row)));
+            x++;
+        }
+        return alphaCells;
+    }
+}
---
+++ src/GameLogic.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+public class GameLogic {
+    private GameHelper helper = new GameHelper();
+    private ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
+
+    public static void main(String[] args) {
+        GameLogic game = new GameLogic();
+        game.setUpGame();
+        game.startPlayning();
+    }
+
+    private void setUpGame() {
+        String[][] dotComName = {
+                {"youtube.com", "github.com", "yoursite.com"},
+                {"behance.com", "adobe.com", "upwork.com"},
+                {"google.com", "jetbrains.com", "samsung.com"}};
+        int randomIntForChoise = (int) (Math.random() * 3);
+        DotCom com1 = new DotCom();
+        com1.setName(dotComName[0][randomIntForChoise]);
+        DotCom com2 = new DotCom();
+        com2.setName(dotComName[1][randomIntForChoise]);
+        DotCom com3 = new DotCom();
+        com3.setName(dotComName[2][randomIntForChoise]);
+        dotComList.add(com1);
+        dotComList.add(com2);
+        dotComList.add(com3);
+
+        System.out.printf("У вас есть три сайта: %s, %s и %s\n", com1.getName(), com2.getName(), com3.getName());
+        System.out.print("Ваша задача- их ддоснуть!\nУдачи!\n");
+
+        for (DotCom site : dotComList) {
+            ArrayList<String> newLoc = helper.placeDotCom(3);
+            site.setLocationCells(newLoc);
+        }
+    }
+
+    private void startPlayning() {
+        while (!dotComList.isEmpty()) {
+            String guess = helper.getUserInput("Введите свою догадку");
+            checkUserGuess(guess);
+        }
+        finishGame();
+    }
+
+    private void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+        String result = "Мимо";
+        for (DotCom site : dotComList) {
+            result = site.checkYourSelf(userGuess);
+            if (result.equals("Попал")) {
+                break;
+            }
+
+            if (result.equals("Потопил")) {
+                dotComList.remove(site);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame() {
+        System.out.println("Вы закончили игру!");
+        if (numOfGuesses < 10) {
+            System.out.printf("Ваше колличесвто попыток- %d. Поздравляем вас!", numOfGuesses);
+        }
+        else {
+            System.out.printf("Ваше колличесвто попыток- %d... Даже моя бабушка бы быстрее эту игру закончила.", numOfGuesses);
+        }
+    }
+}
